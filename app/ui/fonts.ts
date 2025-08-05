30f56e56aac4250934797591600ae5b0f2d29928
Гулевич Екатерина <meteorgul@gmail.com>
2025-08-05 17:36:01 +0300

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Megrim, Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const megrim = Megrim({ subsets: ['latin'], weight: '400'});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']});
